f2f3db61e641ccf0b390a219929875f103f4a362
Sushil K Sharma <sushil.swh@gmail.com>
2023-12-20 18:32:14 +0530

    Create 1-listfiles.py

---
+++ 1-listfiles.py
@@ -0,0 +1,13 @@
+#Python script to list all files in a directory with a specific extension
+
+import os
+
+def list_files_with_extension(directory, extension):
+    files = [file for file in os.listdir(directory) if file.endswith(extension)]
+    return files
+
+# Example usage:
+directory_path = "/path/to/directory"
+file_extension = ".txt"
+result = list_files_with_extension(directory_path, file_extension)
+print(result)
